fb4ca06f3f7c08eb80df0387fa6bc2a01b42eaed
Valter Junior <valter.machado.junior@outlook.com>
2026-03-03 15:24:25 -0300

    usando datetime object no timestamp, caso dê erro ao passar no banco, reverter commit anterior a esse

--- extracao.py
+++ extracao.py
@@ -4,13 +4,12 @@ from datetime import datetime, timedelta
 from conexaoAPI import request_api
 
 
-def transform_data(raw_data, currency="BRL"):
+def parsing_data(raw_data, currency="BRL"):
     final_data = []
 
     for coin in raw_data:
         date = datetime.strptime(coin["quote"][currency]["last_updated"], "%Y-%m-%dT%H:%M:%S.%fZ")
         date_gmt3 = date - timedelta(hours=3)
-        final_date = date_gmt3.strftime("%d/%m/%Y %H:%M:%S")
 
         coin_dict = {
             "name": coin["name"],
@@ -23,7 +22,7 @@ def transform_data(raw_data, currency="BRL"):
             "market_cap": coin["quote"][currency]["market_cap"],
             "percent_change_24h": coin["quote"][currency]["percent_change_24h"],
             "volume_24h": coin["quote"][currency]["volume_24h"],
-            "timestamp": final_date
+            "timestamp": date_gmt3
         }
         final_data.append(coin_dict)
 
@@ -31,6 +30,6 @@ def transform_data(raw_data, currency="BRL"):
 
 
 data = request_api(100, "BRL")
-full_data = transform_data(data)
-print(json.dumps(full_data, indent=4))
+full_data = parsing_data(data)
+print(full_data)
 
